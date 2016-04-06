277bf12dce9a96ed01661c7d504fb6f0954a4fa6
nick arrasate and peter beach <peter@>
2016-04-06 15:56:25 -0700

    add basic styling and toggle player turn divs. added game over event.

--- js/scripts.js
+++ js/scripts.js
@@ -2,37 +2,39 @@
 $(document).ready(function() {
 
 
+
   var total = 0;
   var turnCount = 2;
   var player = null;
   var npc = null;
 
+
   //roll the dice
   $('form#diceRoll').submit(function(event){
     event.preventDefault();
+
+    $('.bust').hide();
     var dice = parseInt(rng(1,6).toPrecision(1));
     total = total + dice;
     $("#display-number").text(dice);
-    // if (dice == 1){
-    //   alert('you lose');
-    //   total = 0;
-    // }
+    if (dice == 1){
+      total = 0;
+      turnCount++;
+      $('.bust').show();
+      $('#hideme1').toggle();
+      $('#hideme2').toggle();
+    }
     $('#floatDisplay').text(total);
-
-
     if (turnCount % 2 == 0 && player == null){
       player = new Scores(total);
     } else if (turnCount % 2 != 0 && npc == null){
       npc = new Scores(total);
     }
-
     if (turnCount % 2 == 0 && player != null){
       player.playerFloat = total;
     } else if (turnCount % 2 != 0 && npc != null){
       npc.playerFloat = total;
     }
-
-
   });
 
   $('form#stay').submit(function(event){
@@ -40,22 +42,42 @@ $(document).ready(function() {
 
     if (turnCount % 2 == 0){
       player.playerTotal += player.addScores();
-      console.log(player.playerTotal);
+      $("#display-score-player").text(player.playerTotal);
+      if (player.playerTotal >= 50){
+        $('#winspan').text('Player 1');
+        $('#winner').show();
+        $('#hideme1').hide();
+        $('#hideme2').hide();
+        var answer = newGame();
+        console.log(answer);
+        if (answer == true){
+          document.location.reload(true);
+        }
+      }
     } else {
       npc.playerTotal += npc.addScores();
-      console.log(npc.playerTotal);
+      $("#display-score-npc").text(npc.playerTotal);
+      if (npc.playerTotal >= 50){
+        $('#winspan').text('NPC');
+        $('#winner').show();
+        $('#hideme1').hide();
+        $('#hideme2').hide();
+        var answer = newGame();
+        if (answer == true){
+          document.location.reload(true);
+        }
+      }
     }
     turnCount++
     total = 0;
+    $('#hideme1').toggle();
+    $('#hideme2').toggle();
     $('#floatDisplay').text('');
     $("#display-number").text('Roll');
-    $("#display-score-player").text(player.playerTotal);
-    $("#display-score-npc").text(npc.playerTotal);
   });
-
-  // convert score to perma score + pass the turn
-
 });
+
+
 
 // Buissness logic
 function Scores(floatingScore){
@@ -68,4 +90,8 @@ Scores.prototype.addScores = function() {
 }
 function rng(min,max) {
   return Math.random() * (max-min) + min;
+}
+function newGame(){
+  var gameReset = confirm('Would you like to play again?');
+  return gameReset;
 }
